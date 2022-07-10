a5ab5685c7c7323b95f4214010922827893c8105
Jill <jillingfromafar@gmail.com>
2022-07-09 21:07:06 -0700

    motified nomoresteak command

--- src/commands/nomoresteak.ts
+++ src/commands/nomoresteak.ts
@@ -1,14 +1,31 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, TextChannel } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
   .setName('nomoresteak')
-  .setDescription(
-    'when you lost your steak after put it in the freezer for 2 days',
-  );
+  .setDescription('steak is gone after 2 days putting it in the freezer');
 
 export async function execute(interaction: CommandInteraction) {
-  return interaction.reply(
-    'My steak in duolingo is no more <:baldtina:975234600160870422>',
-  );
+  const channel = (await interaction.client.channels.fetch(
+    interaction.channel!.id,
+  )) as TextChannel;
+
+  const webhooks = await channel.fetchWebhooks();
+  let webhook = webhooks.find((w) => w.token != null);
+
+  if (!webhook) {
+    webhook = await channel.createWebhook('Bald Macaron', {
+      avatar: interaction.client.user!.avatarURL(),
+    });
+  }
+
+  // TODO: figure out how to bypass mandatory reply for interactions when sending webhook
+  interaction.reply('​');
+  await interaction.deleteReply();
+
+  return await webhook!.send({
+    avatarURL: 'https://cdn.discordapp.com/attachments/992252763369128005/995540026697973800/Cheryl-1.png',
+    username: 'Jill is chilling nearby',
+    content: 'My steak in duolingo is no more <:baldtina:975234600160870422>',
+  });
 }
