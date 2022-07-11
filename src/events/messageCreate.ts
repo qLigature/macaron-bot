b8e510084614cd0972116243b3d65556cc17b21f
qligatore <wilsonjacob.reyes@volenday.com>
2022-07-12 00:37:51 +0800

    slight refactoring and linting

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,25 +1,26 @@
-import {Client, Message, TextChannel, Webhook} from 'discord.js';
+import { Client, Message, TextChannel } from 'discord.js';
 
 module.exports = async (client: Client, message: Message) => {
-    
-    if (message.content.toLowerCase().includes(":baldmeltypet:")) {
-        message.delete();
-        
-        const channel = message.channel as TextChannel;
-        const webhooks = await channel.fetchWebhooks();
-        let webhook = webhooks.find((w) => w.token != null);
+  // TODO: map :emotename: to <...> equivalent with emoteMap module
+  if (message.content.toLowerCase().includes(':baldmeltypet:')) {
+    message.delete();
 
-        if (!webhook) {
-            webhook = await channel.createWebhook('Bald Macaron', {
-            avatar: client.user?.displayAvatarURL(),
-        });
+    const channel = message.channel as TextChannel;
+    const webhooks = await channel.fetchWebhooks();
+    let webhook = webhooks.find((w) => w.token != null);
+
+    if (!webhook) {
+      webhook = await channel.createWebhook('Bald Macaron', {
+        avatar: client.user?.displayAvatarURL(),
+      });
     }
+
+    const { author, content } = message;
 
     return await webhook!.send({
-        avatarURL: message.author.displayAvatarURL(),
-        username: message.author.username,
-        content: message.content,
+      avatarURL: author.displayAvatarURL(),
+      username: author.username,
+      content,
     });
-
-    }
-}
+  }
+};
