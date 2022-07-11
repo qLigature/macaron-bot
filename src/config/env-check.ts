493784d7a71f517c7643a2e74da64eeb2a60370c
Weepy <imbrued.mitre@gmail.com>
2022-07-11 15:28:13 +0800

    more refactoring of names and structure

--- src/config/env-check.ts
+++ src/config/env-check.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 const { CLIENT_TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
 if (!CLIENT_TOKEN || !CLIENT_ID || !GUILD_ID) {
-  throw new Error('Missing environment variables!');
+  throw new Error(
+    'Missing environment variables! Please check your .env file and try again.',
+  );
 }
 
 const envTokens: Record<string, string> = {
