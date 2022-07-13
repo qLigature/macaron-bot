ce547d2a1713b146da1f3402e9e22c46647a0d5d
ThatMajesticGuy <thatmajesticguy420@gmail.com>
2022-07-13 15:50:19 -0500

    [NQN] Actually made the feature function, and added reply feature

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,50 +1,66 @@
-import { Client, Message, TextChannel } from 'discord.js';
-import { getMap } from '../util/map-emoji';
-const regex = new RegExp(String.raw`<a?:\w+:\d+>|(?<!\\):(\w+):`, 'g');
+import {Client, Message, TextChannel, MessageEmbed} from 'discord.js';
+import {getMap} from '../util/map-emoji';
+const regex = new RegExp(String.raw`<a?:\w+:\d+>|(?<!\\):(\w+):`, 'g')
 import * as _ from 'lodash';
-import { exportGuild } from '../models/guild';
+import {exportGuild} from '../models/guild';
 
 module.exports = async (client: Client, message: Message) => {
-  // REPLACE EMOJI
-  if (message.author.bot) return;
-  let emojis = message.content.match(regex);
+    // REPLACE EMOJI
+    if (message.author.bot) return;
+    if (message.channel.type === "DM") return;
+    let emojis = message.content.match(regex);
 
-  if (emojis == null) return;
+    if (emojis == null) return;
 
-  emojis = _.uniq(emojis);
-  const emojiMap = getMap();
+    emojis = _.uniq(emojis);
+    const emojiMap = getMap();
 
-  let newMsg = message.content;
+    let newMsg = message.content;
 
-  emojis!.forEach(async (em) => {
-    const emoji = emojiMap.get(em);
-    if (!emoji) return;
+    for (var i = 0; i < emojis.length; i++) {
+        let em = emojis[i]
+        const emoji = emojiMap.get(em)
+        if (!emoji) return;
 
-    const guild = await exportGuild(emoji.guild.id);
-    if (!guild.opt) return;
+        const guild = await exportGuild(emoji.guild.id)
+        if (!guild.opt) return console.log("emoji opt out");
 
-    newMsg = newMsg.replaceAll(em, emojiMap.get(em));
-  });
+        newMsg = newMsg.replaceAll(em, emojiMap.get(em))
+    }
 
-  // send emojis
-  if (newMsg == message.content) return;
-  if (message.guild?.me?.permissions.has('MANAGE_MESSAGES')) {
-    message.delete();
-  }
+    // send emojis
+    if (newMsg == message.content) return;
 
-  const channel = message.channel as TextChannel;
-  const webhooks = await channel.fetchWebhooks();
-  let webhook = webhooks.find((w) => w.token != null);
+    if (message.guild?.me?.permissions.has("MANAGE_MESSAGES")) {message.delete()}
 
-  if (!webhook) {
-    webhook = await channel.createWebhook('Bald Macaron', {
-      avatar: client.user?.avatarURL(),
+    const channel = message.channel as TextChannel;
+    const webhooks = await channel.fetchWebhooks();
+    let webhook = webhooks.find((w) => w.token != null);
+
+    if (!webhook) {
+        webhook = await channel.createWebhook('Bald Macaron', {
+            avatar: client.user?.avatarURL(),
+        });
+    }
+
+    let embeds = [];
+    if (message.reference != null) {
+        const reply = await message.fetchReference();
+
+        if (reply.content.length > 900) reply.content = reply.content.substring(0, 950) + "..."
+        const embed = new MessageEmbed()
+        .setColor("#414987")
+        .setAuthor({name: reply.author.tag, iconURL: reply.author.displayAvatarURL()})
+        .setDescription(reply.content + `\n\n[Jump to Message](https://discord.com/channels/${reply.guildId}/${reply.channelId}/${reply.id})`)
+        embeds.push(embed)
+
+    }
+
+    return await webhook!.send({
+        avatarURL: message.author.displayAvatarURL(),
+        username: message.author.username,
+        content: newMsg,
+        embeds: embeds
     });
-  }
+}
 
-  return await webhook!.send({
-    avatarURL: message.author.displayAvatarURL(),
-    username: message.author.username,
-    content: newMsg,
-  });
-};
