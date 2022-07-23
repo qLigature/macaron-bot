651f9d723c5f90e1fb0266410696111a8e97151c
Weepy <imbrued.mitre@gmail.com>
2022-07-23 14:00:38 +0800

    fixed play command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -39,14 +39,21 @@ export const data = new SlashCommandBuilder()
           .setRequired(true),
       ),
   );
+
 export async function execute(interaction: CommandInteraction) {
-  if (!interaction.member!.voice.channel)
+  const guild = interaction.client.guilds.cache.get(interaction.guildId!);
+  const member = guild!.members.cache.get(interaction.member!.user.id);
+  const voiceChannel = member!.voice.channel;
+
+  await interaction.reply('Playing something...');
+
+  if (!voiceChannel)
     return interaction.editReply(
       'Beep boop! You need to be in a VC to use this command',
     );
 
   const queue = await interaction.client.player.createQueue(interaction.guild!);
-  if (!queue.connection) await queue.connect(interaction.member!.voice.channel);
+  if (!queue.connection) await queue.connect(voiceChannel!);
 
   const embed = new MessageEmbed();
 
@@ -56,19 +63,21 @@ export async function execute(interaction: CommandInteraction) {
       requestedBy: interaction.user,
       searchEngine: QueryType.YOUTUBE_VIDEO,
     });
+
     if (result.tracks.length === 0) return interaction.editReply('No results');
 
     const song = result.tracks[0];
     await queue.addTrack(song);
-    embed;
-    new MessageEmbed()
-      .setAuthor({ name: 'Added Track' })
-      .setTitle(`${song.title}`)
-      .setURL(`${song.url}`)
+
+    embed
+      .setDescription(
+        `**[${song.title}](${song.url})** has been added to the Queue`,
+      )
       .setThumbnail(song.thumbnail)
       .setFooter({ text: `Duration: ${song.duration}` });
   } else if (interaction.options.getSubcommand() === 'playlist') {
     const url = interaction.options.getString('url');
+
     const result = await interaction.client.player.search(url!, {
       requestedBy: interaction.user,
       searchEngine: QueryType.YOUTUBE_PLAYLIST,
@@ -96,6 +105,7 @@ export async function execute(interaction: CommandInteraction) {
 
     const song = result.tracks[0];
     await queue.addTrack(song);
+
     embed
       .setDescription(
         `**[${song.title}](${song.url})** has been added to the Queue`,
@@ -103,6 +113,7 @@ export async function execute(interaction: CommandInteraction) {
       .setThumbnail(song.thumbnail)
       .setFooter({ text: `Duration: ${song.duration}` });
   }
+
   if (!queue.playing) await queue.play();
   await interaction.editReply({
     embeds: [embed],
