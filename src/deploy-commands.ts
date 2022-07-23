b238ef527c380c306851b006910a1772c4c60c34
Weepy <imbrued.mitre@gmail.com>
2022-07-23 14:00:16 +0800

    moved player initializing to index.ts

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,29 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Client } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import envTokens from './config/env-check';
 import * as commandModules from './commands';
-import { Player } from 'discord-player';
 
-type Command = {
-  data: any;
-};
+// type Command = {
+//   data: any;
+// };
 
 const commands: any = [];
 
-const client = new Client({
-  intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'],
-});
-
 const { CLIENT_TOKEN, CLIENT_ID, GUILD_ID } = envTokens;
-
-client.player = new Player(client, {
-  ytdlOptions: {
-    quality: 'highestaudio',
-    highWaterMark: 1 << 25,
-  },
-});
 
 for (const module of Object.values<any>(commandModules)) {
   commands.push(module.data);
