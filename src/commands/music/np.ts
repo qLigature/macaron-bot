a75dde58d04801f55da9999a64d288dedae78a23
ThatMajesticGuy <thatmajesticguy420@gmail.com>
2022-07-27 13:59:25 -0500

    More music commands

---
+++ src/commands/music/np.ts
@@ -0,0 +1,25 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+    .setName('np')
+    .setDescription('Displays info about the currently playing song')
+
+export const execute =  async (interaction: CommandInteraction) => {
+    const queue = interaction.client.player.getQueue(interaction.guildId!);
+
+    if (!queue) return await interaction.reply('Beep Boop! No song detected.');
+
+    const song = queue.current
+
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setAuthor({ name: 'Track Info' })
+          .setTitle(`${song.title}`)
+          .setURL(`${song.url}`)
+          .setThumbnail(song.thumbnail)
+          .setDescription(`Currently Playing [${song.title}](${song.url})\n${queue.createProgressBar()}`),
+      ],
+    });
+  }
