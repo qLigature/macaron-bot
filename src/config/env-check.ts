200160d43c1c799b44aa3d017ad93cfdb874d031
Weepy <imbrued.mitre@gmail.com>
2022-07-31 00:31:17 +0800

    added node_env var

--- src/config/env-check.ts
+++ src/config/env-check.ts
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { CLIENT_TOKEN, CLIENT_ID, GUILD_ID, MONGODB_URI } = process.env;
+const { NODE_ENV, CLIENT_TOKEN, CLIENT_ID, GUILD_ID, MONGODB_URI } =
+  process.env;
 
-if (!CLIENT_TOKEN || !CLIENT_ID || !GUILD_ID || !MONGODB_URI) {
+if (!NODE_ENV || !CLIENT_TOKEN || !CLIENT_ID || !GUILD_ID || !MONGODB_URI) {
   throw new Error(
     'Missing environment variables! Please check your .env file and try again.',
   );
 }
 
 const envTokens: Record<string, string> = {
+  NODE_ENV,
   CLIENT_TOKEN,
   CLIENT_ID,
   GUILD_ID,
